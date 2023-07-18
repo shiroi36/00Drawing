9b72c8f1d5a857c6d0973490540de94aa4b6e71d
shiroi36 <keita104824@gmail.com>
2023-07-18 10:01:21 +0900

    2023年7月18日継手リスト追加

--- log.bat
+++ log.bat
@@ -6,7 +6,7 @@ rem この呪文を行うにはgithubcliのインストールが必要です。 https://cli.github.com
 rem 途中でautholityの認証があったが、指示に従って楽勝だった
 
 rem 新三芳はissue#1とした
-gh issue view 1 > 新三芳履歴.txt
+gh issue view 125 > 2023年7月12日.md
 
 pause
 exit
